fb6b88cc43692ef25ac5ba89fd5f4b94fe181680
katieFgh@gmail.com <32751159@katiefgh@users.noreplay.github.com>
2019-12-09 18:46:42 -0600

    adding files

--- studio/index.ts
+++ studio/index.ts
@@ -4,3 +4,7 @@
 // TODO: 
 // * Code the Astronaut, Cargo, and Rocket classes in new files.
 // * Import the three classes into this file.
+
+import { Astronaut } from './Astronaut.ts';
+import { Cargo } from './Cargo.ts';
+import { Rocket } from './Rocket.ts';
